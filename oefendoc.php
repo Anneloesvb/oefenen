fb54195495926c5a7588258792d2622ae9a99453
Anneloesvb <anneloesvanbladel@gmail.com>
2024-06-26 10:38:25 +0200

    afbeelding

--- oefendoc.php
+++ oefendoc.php
@@ -1,5 +1,12 @@
 <link rel="stylesheet" type="text/css" href="oefenstyle.css" />
-
+<div class="blok">
+  <h1 class="naam">Leeuw</h1>
+  <img src="afbeeldingen/leeuw.jpg" width="100%">
+  <p class="Info">Afrikaanse leeuwen leven in het grootste gedeelte van de sub-Sahara van Afrika, in open landschappen met gras. Ooit werden leeuwen uitgeroeid vanuit Zuid-Afrika. Daarom leven ze daar nu in nationale parken zoals het Kruger National Park en Kalahari Gemsbok National Park, waar ze zo goed mogelijk beschermd zijn. Vroeger kwamen leeuwen voor tussen Zuidwest-Azië en Noord-Afrika.</p>
+  <p class="Woonplaats">Afrika</p>
+  <button class="knop">LEES MEER</button>
+</div>
+<hr>
 <?php
 
 // ++ is plus 1 / + is erbij / . punt is aan elkaar plakken
@@ -42,8 +49,11 @@ $mysqli = new mysqli("localhost","root","","anneloes");
 $sql = "SELECT * FROM dieren";
 $result = $mysqli -> query($sql); // t/m hier is de code die je nodig hebt om php te connecten met sql. Verander wel de database naam, de table naam etc. (in dit geval: root / anneloes en dieren)
 while($row = $result -> fetch_assoc()){ 
-  echo '<div class="blok"><h1 class="naam">' .($row["Naam"]) .'</h1>';
-  echo '<img src="afbeeldingen/'.($row["Afbeelding"]) .'">';
+  echo '<div class="blok"><h1 class="naam">HET DIER: de ' .($row["Naam"]) .':</h1>';
+                              // <h1 class="naam">Leeuw</h1>
+  echo '<img src="afbeeldingen/'.($row["Afbeelding"]) .'" width="100%">';
+
+  
   echo '<p class="Info">' .($row["Info"]) .'</p>';
   echo '<p class="Woonplaats">' .($row["Woonplaats"]) .'</p>';
   echo '<button class="knop">LEES MEER</button></div>';
